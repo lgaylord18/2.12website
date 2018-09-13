4d65d86bebe0b6d880bf5ca3eda6c7888899cc40
lgaylord18 <lgaylord18@gmail.com>
2018-09-13 12:33:12 -0400

    Add files via upload

---
+++ php/footer.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <footer class="page-section bg-gray-lighter footer pb-0 pt-50">
+		<div class="container">
+            <div class="footer-social-links pb-30">
+				<a style="margin-right:50px" href="https://github.com/lgaylord18" title="Github" target="_blank"><i class="fa fa-github fa-5x"></i></a>
+                <a style="margin-left:50px" href="https://www.linkedin.com/in/elizabeth-gaylord-009a49ab" title="LinkedIn+" target="_blank"><i class="fa fa-linkedin fa-5x"></i></a>
+            </div>
+                
+            <div class="local-scroll">
+				<a href="#top" class="link-to-top"><i class="fa fa-caret-up"></i></a>
+            </div>
+		</div>   
+    </footer>
---
+++ php/header.php
@@ -0,0 +1,25 @@
+<!doctpye html>
+<html>
+    <head>
+        <title>Liza Gaylord</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta charset="utf-8">
+        <meta name="author" content="Roman Kirichik">
+        <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+        
+        <!-- Favicons -->
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" type="image/png" href="images/favicon.png">
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/style-responsive.css">
+        <link rel="stylesheet" href="css/animate.min.css">
+        <link rel="stylesheet" href="css/vertical-rhythm.min.css">
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        <link rel="stylesheet" href="css/magnific-popup.css">        
+        
+    </head>
